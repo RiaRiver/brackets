3fb55636a9de995abf452ca524550934ee7c72ed
Ria River <riria.river@gmail.com>
2023-10-25 15:38:02 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketPairs = Object.fromEntries(bracketsConfig);
+
+  const isOpenCloseSame = (bracket) => bracket === bracketPairs[bracket];
+  // eslint-disable-next-line no-prototype-builtins
+  const isOpenBracket = (bracket) => bracketPairs.hasOwnProperty(bracket);
+  const stack = [];
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const bracket of str) {
+    if (isOpenCloseSame(bracket)) {
+      const stacked = stack.at(-1);
+      if (stacked !== bracket) {
+        stack.push(bracket);
+      } else stack.pop();
+    } else if (isOpenBracket(bracket)) {
+      stack.push(bracket);
+    } else {
+      const stacked = stack.pop();
+      if (bracketPairs[stacked] !== bracket) return false;
+    }
+  }
+
+  return !stack.length;
+};
